3cf9a15d459a37d12be2ffc0aca1e8ddd828e1c1
根津宏輔 <kousuke@nezusuke.local>
2018-12-20 12:04:07 +0900

    add Main,MainFrame

---
+++ Main.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+/**
+ * Main
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    MainFrame mineFrame = new MainFrame();
+    mineFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    mineFrame.setVisible(true);
+  }
+}
---
+++ MainFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+/**
+ * MineFrame
+ */
+public class MainFrame extends JFrame {
+
+  MainFrame() {
+    super("Minesweeper");
+    setBounds(100, 100, 800, 600);
+
+  }
+}
