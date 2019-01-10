f2ca32798671de05f23b05947de22553556c4ace
根津宏輔 <kousuke@nezusuke.local>
2019-01-10 13:06:46 +0900

    ゲームの基礎完成

--- GamePanel.java
+++ GamePanel.java
@@ -16,29 +16,35 @@ public class GamePanel extends JPanel {
   int width;
   int height;
   int bombnumber;
+
+  // 以下３つのfieldは周囲に余分に一桝関係ないマスを置いているので注意。壁用。
   // 0: 何もなし、-1: 爆弾、1以上: 周囲の爆弾の数
   int[][] fieldData;
-  // 1: 空いている、0：空いていない
+  // -1: 空いている、0：空いていない、1以上：周囲の爆弾の数
   int[][] openField;
   JButton[][] field;
+
   boolean notClickedFlag = true;
 
   GamePanel(MainFrame mf, String difficulty) {
     mainFrame = mf;
-    if (difficulty == "easy") {
-      width = 10;
-      height = 15;
-      bombnumber = 20;
-      fieldData = new int[height][width];
-      openField = new int[height][width];
-      field = new JButton[height][width];
+    if (difficulty == mainFrame.GAME_DIFFICULTY[0]) {
+      width = 8;
+      height = 8;
+      bombnumber = 10;
+      fieldData = new int[height + 2][width + 2];
+      openField = new int[height + 2][width + 2];
+      field = new JButton[height + 2][width + 2];
       setLayout(new GridLayout(width, height));
     }
 
-    for (int y = 0; y < height; y++) {
-      for (int x = 0; x < width; x++) {
+    for (int y = 0; y < height + 2; y++) {
+      for (int x = 0; x < width + 2; x++) {
         field[y][x] = new JButton();
         field[y][x].setActionCommand(y + ":" + x);
+        if (y == 0 || y == height + 1 || x == 0 || x == width + 1) {
+          continue;
+        }
         add(field[y][x]);
 
         field[y][x].addActionListener(new ActionListener() {
@@ -50,16 +56,18 @@ public class GamePanel extends JPanel {
             int x = Integer.parseInt(splitedCommand[1]);
             int y = Integer.parseInt(splitedCommand[0]);
 
-            openField[y][x] = 1;
             if (notClickedFlag) {
+              notClickedFlag = !notClickedFlag;
               construct(y, x);
-              // 数字の計算
+              calculate();
+              printField();
             }
 
-            field[y][x].setEnabled(false);
             if (fieldData[y][x] == -1) {
+              // ゲームオーバー処理
               mainFrame.panelChange(mainFrame.panelNames[0]);
             }
+            openCell(x, y);
           }
         });
       }
@@ -69,9 +77,9 @@ public class GamePanel extends JPanel {
   void construct(int y, int x) {
     int constructedBombNumber = 0;
     while (constructedBombNumber < bombnumber) {
-      int constructedX = (int) (Math.random() * width);
-      int constructedY = (int) (Math.random() * height);
-      if (constructedX == x && constructedY == y) {
+      int constructedX = (int) (Math.random() * width) + 1;
+      int constructedY = (int) (Math.random() * height) + 1;
+      if (constructedX >= x - 1 && constructedX <= x + 1 && constructedY >= y - 1 && constructedY <= y + 1) {
         continue;
       }
       if (fieldData[constructedY][constructedX] == -1) {
@@ -79,6 +87,69 @@ public class GamePanel extends JPanel {
       }
       fieldData[constructedY][constructedX] = -1;
       constructedBombNumber++;
+    }
+  }
+
+  void calculate() {
+    for (int y = 0; y < height + 2; y++) {
+      for (int x = 0; x < width + 2; x++) {
+        if (fieldData[y][x] == -1) {
+          for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+              if (fieldData[y + i][x + j] == -1) {
+                continue;
+              }
+              fieldData[y + i][x + j]++;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  void openCell(int x, int y) {
+    System.out.println(x + "," + y);
+    if (x < 1 || y < 1 || x >= width + 1 || y >= height + 1) {
+      System.out.println("a");
+      return;
+    }
+    if (openField[y][x] == -1) {
+      System.out.println("b");
+      return;
+    }
+    if (fieldData[y][x] == -1) {
+      System.out.println("c");
+      return;
+    }
+    if (fieldData[y][x] == 0) {
+      System.out.println("d");
+      openField[y][x] = -1;
+      field[y][x].setEnabled(false);
+    }
+    if (fieldData[y][x] > 0) {
+      System.out.println("e");
+      openField[y][x] = -1;
+      field[y][x].setText(String.valueOf(fieldData[y][x]));
+      field[y][x].setEnabled(false);
+      return;
+    }
+
+    for (int indexY = -1; indexY < 2; indexY++) {
+      for (int indexX = -1; indexX < 2; indexX++) {
+        if (indexX == 0 && indexY == 0) {
+          continue;
+        }
+        openCell(x + indexX, y + indexY);
+      }
+    }
+  }
+
+  void printField() {
+    for (int y = 0; y < height + 2; y++) {
+      for (int x = 0; x < width + 2; x++) {
+        System.out.print(fieldData[y][x] + ", ");
+      }
+      System.out.println();
     }
   }
 }
