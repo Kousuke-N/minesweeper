231ca0f93d70ebd468a461228baa934ce31b5584
根津宏輔 <kousuke@nezusuke.local>
2019-01-24 11:55:42 +0900

    ゲームリザルト画面実装

--- components/layout/GamePanel.java
+++ components/layout/GamePanel.java
@@ -1,24 +1,14 @@
 package components.layout;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.Arrays;
 
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import components.MainFrame;
@@ -110,7 +100,7 @@ public class GamePanel extends JPanel {
     super.paint(g);
   }
 
-  public void gameover() {
-    mainFrame.panelChange(mainFrame.panelNames[4], false);
+  public void toResult(boolean result) {
+    mainFrame.panelChange(mainFrame.panelNames[4], result);
   }
 }
